0dbcb0af4c7e7369eefd8ca6da1dd8b5883a74e6
mahesh <59368762+maheshch123@users.noreply.github.com>
2020-05-01 14:13:38 +0530

    Update views.py
    changes done

--- weather/weatherapp/views.py
+++ weather/weatherapp/views.py
@@ -4,22 +4,22 @@ import requests
 # Create your views here.
 
 def index(request):
-    url = 'http://api.openweathermap.org/data/2.5/weather?q={}&units=metric&APPID=3842242fbbed128879e5973bbc483a75'
-    city='hyderabad'
-    r= requests.get(url.format(city)).json()
-   
-    
-    
-    
-    weather = {
-        'city': city.lower(),
-        'temperature': r['main']['temp'],
-        'description': r['weather'][0]['description'],
-        'icon': r['weather'][0]['icon'],
-        'wind': r['wind']['speed'],
-        'country':r['sys']['country']
-    }
-    print(weather)
-    context = {'weather':weather}
-    return render(request, 'weather.html',context)
+    if request.method == 'GET':
+        url = 'http://api.openweathermap.org/data/2.5/weather?q={}&units=metric&APPID=3842242fbbed128879e5973bbc483a75'
+        city = request.GET.get('city')
+        r= requests.get(url.format(city)).json()
+            
+        weather = {
+            'city': city,
+            'temperature': r['main']['temp'],
+            'description': r['weather'][0]['description'],
+            'icon': r['weather'][0]['icon'],
+            'wind': r['wind']['speed'],
+            'country':r['sys']['country']
+            }
+
+        print(weather)
+        return render(request, 'weather.html',{'weather':weather},)
+           
+
     
